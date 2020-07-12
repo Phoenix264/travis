07602f13f8a868d1964eb43720910f7df8819cf5
Alexander <mysticphoenix@mail.ru>
2020-07-12 19:42:08 +0300

    Инициализация

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include "version.h"
+
+using namespace std;
+
+int main()
+{
+    cout << "build " << PROJECT_VERSION_PATCH << endl;
+    cout << "Hello, World!" << endl;
+    return 0;
+}
